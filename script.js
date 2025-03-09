5e04b9fbb0ea5a27c646f909a458fe1d68a8538e
Palakurthi Sowmya <111228519+Sowmyapalakurthi@users.noreply.github.com>
2025-03-09 12:50:01 +0530

    Created script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Import the figlet library
+const figlet = require('figlet');
+
+// Use figlet to generate ASCII art
+figlet('Hello World!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
